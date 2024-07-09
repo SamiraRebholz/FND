6a5d160ae1594d1b6c8c9cc74a8e21ce8492d0fa
SamiraRebholz <147277292+SamiraRebholz@users.noreply.github.com>
2024-07-09 18:41:45 +0000

    Add content and styling classes

--- Features/Navigation/navigation.js
+++ Features/Navigation/navigation.js
@@ -6,9 +6,8 @@ class MyNavbar extends HTMLElement {
             <li><a href="./index.html"><img src="./Assets/icons/home.svg" alt="home" class="noOverlay"></a></li>
             <li class="hidden menu"><a><img src="./Assets/icons/menu.svg" alt="menu" class="noOverlay"></a></li>
             <li><a href="./symptoms.html">Diagnostik & Symptome</a></li>
-            <li><a href="">Pathophysiologie</a></li>
-            <li><a href="">Epidemiologie</a></li>
-            <li><a href="">Behandlung</a></li>
+            <li><a href="./mechanism.html">Pathophysiologie</a></li>
+            <li><a href="./treatment.html">Behandlung</a></li>
             <li><a href="./sources.html">Quellen</a></li>
             <li><img src="./Assets/icons/settings.svg" class="noOverlay settings"></li>
           </ul>
